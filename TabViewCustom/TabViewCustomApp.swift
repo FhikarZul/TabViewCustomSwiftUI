1928745de3ac466d24cd2ada3cc99b790fb5b761
Fhikar Zul <Fhikarzul18@gmail.com>
2022-09-13 15:26:57 +0800

    Initial Commit

---
+++ TabViewCustom/TabViewCustomApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewCustomApp.swift
+//  TabViewCustom
+//
+//  Created by Lucy on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct TabViewCustomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
